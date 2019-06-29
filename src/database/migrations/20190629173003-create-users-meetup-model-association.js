92523714c38fdf6f6e3a5d6cfea2c276e720eeec
AugustoMarcelo <mrclgst10@gmail.com>
2019-06-29 15:04:36 -0300

    Criação de tabela para associação de users e meetups

---
+++ src/database/migrations/20190629173003-create-users-meetup-model-association.js
@@ -0,0 +1,24 @@
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        return queryInterface.createTable('meetup_subscriptions', {
+            user_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'users', key: 'id' },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+            meetup_id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                references: { model: 'meetups', key: 'id' },
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            },
+        });
+    },
+
+    down: queryInterface => {
+        return queryInterface.dropTable('meetup_subscriptions');
+    },
+};
