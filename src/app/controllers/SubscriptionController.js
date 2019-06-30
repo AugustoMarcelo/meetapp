ef02246441f17145337497c4d64dea4b7b84f38c
AugustoMarcelo <mrclgst10@gmail.com>
2019-06-30 16:59:38 -0300

    Método de listagem criado

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -3,8 +3,38 @@ import { Op } from 'sequelize';
 
 import Subscription from '../models/Subscription';
 import Meetup from '../models/Meetup';
+import User from '../models/User';
 
 class SubscriptionController {
+    async index(req, res) {
+        const subscriptions = await Subscription.findAll({
+            where: { user_id: req.userId },
+            attributes: ['meetup_id'],
+            order: [['meetup', 'date', 'ASC']],
+            include: [
+                {
+                    model: Meetup,
+                    as: 'meetup',
+                    where: {
+                        date: {
+                            [Op.gt]: new Date(),
+                        },
+                    },
+                    attributes: ['title', 'description', 'date', 'location'],
+                    include: [
+                        {
+                            model: User,
+                            as: 'user',
+                            attributes: ['id', 'name', 'email'],
+                        },
+                    ],
+                },
+            ],
+        });
+
+        return res.json(subscriptions);
+    }
+
     async store(req, res) {
         const schema = Yup.object().shape({
             meetup_id: Yup.number().required(),
