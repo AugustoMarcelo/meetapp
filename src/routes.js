b3de16b93a0c8cd95d1921907deecc01cc82e4c8
AugustoMarcelo <mrclgst10@gmail.com>
2019-06-30 16:24:12 -0300

    Rota para inscrições em meetups adicionada

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
 import UserMeetupsController from './app/controllers/UserMeetupsController';
+import SubscriptionController from './app/controllers/SubscriptionController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -29,6 +30,8 @@ routes.get('/mymeetups', UserMeetupsController.index);
 routes.put('/mymeetups/:id', UserMeetupsController.update);
 // Route to delete meetups
 routes.delete('/mymeetups/:id', UserMeetupsController.delete);
+
+routes.post('/subscriptions', SubscriptionController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
