f30578c217617124fbb31ae22121339c605b1c67
AugustoMarcelo <mrclgst10@gmail.com>
2019-06-29 23:30:41 -0300

    Método index para listagem de meetups adicionado

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,7 +1,42 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Meetup from '../models/Meetup';
+import User from '../models/User';
+import File from '../models/File';
 
 class MeetupController {
+    async index(req, res) {
+        const { page = 1 } = req.query;
+        const meetups = await Meetup.findAll({
+            where: { user_id: req.userId },
+            order: ['date'],
+            attributes: ['id', 'title', 'description', 'location', 'date'],
+            limit: 20,
+            offset: (page - 1) * 20,
+            include: [
+                {
+                    model: User,
+                    as: 'user',
+                    attributes: ['id', 'name', 'email'],
+                    include: [
+                        {
+                            model: File,
+                            as: 'avatar',
+                            attributes: ['id', 'url', 'path'],
+                        },
+                    ],
+                },
+                {
+                    model: File,
+                    as: 'banner',
+                    attributes: ['id', 'url', 'path'],
+                },
+            ],
+        });
+
+        return res.json(meetups);
+    }
+
     async store(req, res) {
         const schema = Yup.object().shape({
             title: Yup.string().required(),
@@ -19,6 +54,17 @@ class MeetupController {
         }
 
         const { title, description, location, date, banner_id } = req.body;
+
+        const hourStart = startOfHour(parseISO(date));
+
+        /**
+         * Check for past dates
+         */
+        if (isBefore(hourStart, new Date())) {
+            return res
+                .status(400)
+                .json({ error: 'Past dates are not permitted' });
+        }
 
         const meetup = await Meetup.create({
             title,
