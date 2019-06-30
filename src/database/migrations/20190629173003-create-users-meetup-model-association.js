469db370000c72528f6cbfadc2c59d7b73daed01
AugustoMarcelo <mrclgst10@gmail.com>
2019-06-30 16:24:52 -0300

    Inserção de colunas id, createdAt e updatedAt na tabela de Subscriptions

--- src/database/migrations/20190629173003-create-users-meetup-model-association.js
+++ src/database/migrations/20190629173003-create-users-meetup-model-association.js
@@ -1,6 +1,12 @@
 module.exports = {
     up: (queryInterface, Sequelize) => {
         return queryInterface.createTable('subscriptions', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+            },
             user_id: {
                 type: Sequelize.INTEGER,
                 allowNull: false,
@@ -14,6 +20,14 @@ module.exports = {
                 references: { model: 'meetups', key: 'id' },
                 onUpdate: 'CASCADE',
                 onDelete: 'CASCADE',
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
             },
         });
     },
