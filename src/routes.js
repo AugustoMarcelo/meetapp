86f2d065a1b709f164adb1aea999f9cfc1cfadde
AugustoMarcelo <mrclgst10@gmail.com>
2019-06-30 13:57:33 -0300

    Listagem, atualização e cancelamento de meetups do usuário logado adicionadas

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import MeetupController from './app/controllers/MeetupController';
+import UserMeetupsController from './app/controllers/UserMeetupsController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,6 +21,13 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.post('/meetups', MeetupController.store);
+// Route to list meetups from logged user
+routes.get('/mymeetups', UserMeetupsController.index);
+// Route to update meetups
+routes.put('/mymeetups/:id', UserMeetupsController.update);
+// Route to delete meetups
+routes.delete('/mymeetups/:id', UserMeetupsController.delete);
+// Route to list meetups
 routes.get('/meetups', MeetupController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
